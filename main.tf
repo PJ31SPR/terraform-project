b3dab27a276c33959882f65e589662552dbc03d8
Patsy <patsysprung@hotmail.co.uk>
2025-04-08 13:13:03 +0100

    adds main.tf and workflow files

---
+++ main.tf
@@ -0,0 +1,13 @@
+# Provider Configuration
+provider "aws" {
+    region = "eu-west-2"  
+}
+
+# Backend Configuration for Remote State
+terraform {
+    backend "s3" {
+        bucket = "patsy-terraform-bucket"  
+        key    = "terraform.tfstate"       # Name of the state file
+        region = "eu-west-2"               
+    }
+}
