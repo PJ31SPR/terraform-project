84ba8bec0c8e131e78cd959069b41c4fac1e9b47
Patsy <patsysprung@hotmail.co.uk>
2025-04-09 15:34:11 +0100

    edits postgres engine version

--- database.tf
+++ database.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "rds_app" {
   allocated_storage    = 10
   engine               = "postgres"
-  engine_version       = "15.3"
+  engine_version       = "11.22"
   instance_class       = "db.t3.micro"
   identifier           = "patsy-task-app"
   db_name                 = "patsy-task-app-database"
